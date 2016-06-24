f72c1540d924a4b6e98c87dec2848178784031e4
AnanasowyOcelot <lukasz.woroniecki@gmail.com>
2016-06-24 17:45:08 +0200

    commons refactor

--- src/main/scala/Utils/Utils.scala
+++ src/main/scala/Utils/Utils.scala
@@ -12,11 +12,9 @@ object Utils {
   abstract class TaskScheduler {
     def schedule[T](body: => T): ForkJoinTask[T]
     def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
-      val right = task {
-        taskB
-      }
+      val right = task(taskB)
       val left = taskA
-      (left, right.join)
+      (left, right.join())
     }
   }
 
@@ -26,17 +24,14 @@ object Utils {
         def compute = body
       }
       Thread.currentThread match {
-        case wt: ForkJoinWorkerThread =>
-          t.fork()
-        case _ =>
-          forkJoinPool.execute(t)
+        case wt: ForkJoinWorkerThread => t.fork()
+        case _ => forkJoinPool.execute(t)
       }
       t
     }
   }
 
-  val scheduler =
-    new DynamicVariable[TaskScheduler](new DefaultTaskScheduler)
+  val scheduler = new DynamicVariable[TaskScheduler](new DefaultTaskScheduler)
 
   def task[T](body: => T): ForkJoinTask[T] = {
     scheduler.value.schedule(body)
@@ -47,15 +42,9 @@ object Utils {
   }
 
   def parallel[A, B, C, D](taskA: => A, taskB: => B, taskC: => C, taskD: => D): (A, B, C, D) = {
-    val ta = task {
-      taskA
-    }
-    val tb = task {
-      taskB
-    }
-    val tc = task {
-      taskC
-    }
+    val ta = task(taskA)
+    val tb = task(taskB)
+    val tc = task(taskC)
     val td = taskD
     (ta.join(), tb.join(), tc.join(), td)
   }
